2c33c3f50a33896b482a77d38631eaee0821d58f
Mahamud <mfo253@email.vccs.edu>
2020-04-12 23:15:31 -0400

    Text buttton video

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Alexa clinic',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1> Take complete eye exams using Amazon Alexa</h1>
+    <p>A complete guide on how to take eye exam using Alexa</p>
+    <Link to="/page-2/">Watch the video</Link>
   </div>
 )
 
 export default IndexPage
+
