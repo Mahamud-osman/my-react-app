42545837dd69bd6593785b1f22264748617c94ec
Mahamud <mfo253@email.vccs.edu>
2020-04-13 01:08:34 -0400

    CSS page, removed content.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import Link from 'gatsby-link'
+  import React from 'react'
+  import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1> Take complete eye exams using Amazon Alexa</h1>
-    <p>A complete guide on how to take eye exam using Alexa</p>
-    <Link to="/page-2/">Watch the video</Link>
-  </div>
-)
+  const IndexPage = () => (
+    <div>
+      <div className = "Hero">
+        <div className = "HeroGroup"> 
+      <h1> Take complete eye exams using Amazon Alexa</h1>
+      <p>A complete guide on how to take eye exam using Alexa</p>
+      <Link to="/page-2/">Watch the video</Link>
+        </div>
+      </div>
+      </div>
+      
+  )
 
 export default IndexPage
 
